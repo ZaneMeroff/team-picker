0c8c47400a833d162e0a3abf8ee4a4f9f38ee6c0
Zachary Nemeroff <zanemeroff@gmail.com>
2020-07-21 12:30:41 -0600

    Create basic skeleton

---
+++ src/components/NumSelector/NumSelector.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>pick a number of teams:</p>
+    <p>0</p>
+    <button>+</button>
+    <button>-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NumSelector',
+  props: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>

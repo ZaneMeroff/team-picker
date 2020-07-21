8bdb5b3536cd34a24412d4891dbe598c36ed33fa
Zachary Nemeroff <zanemeroff@gmail.com>
2020-07-21 17:37:07 -0600

    Create component skeleton

---
+++ src/components/PlayerNameInputScreen/PlayerNameInputScreen.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class='player-name-input-outer-container'>
+    <input placeholder='player 1 name' class='player-name-input'/>
+    <input placeholder='player 2 name' class='player-name-input'/>
+    <input placeholder='player 3 name' class='player-name-input'/>
+    <input placeholder='player 4 name' class='player-name-input'/>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'PlayerNameInputScreen',
+    props: {},
+    data() {
+      return {
+        // code
+      }
+    },
+    methods: {}
+  }
+</script>
+
+<style>
+  @import './PlayerNameInputScreen.css';
+</style>

d9106dae8e223f66a7173f15ffc17c95ec54b0e2
Zachary Nemeroff <zanemeroff@gmail.com>
2020-07-21 12:45:25 -0600

    Create + - buttons

--- src/components/NumSelector/NumSelector.vue
+++ src/components/NumSelector/NumSelector.vue
@@ -2,12 +2,12 @@
   <section>
     <p>pick a number of teams:</p>
     <div class='picker-num-btn-container'>
-      <div>
-        <p>0</p>
+      <div class='team-number-display-container'>
+        <p class='team-number-display'>0</p>
       </div>
       <div class='plus-minus-button-container'>
-        <button>+</button>
-        <button>-</button>
+        <button class='increment-button'>+</button>
+        <button class='increment-button'>-</button>
       </div>
     </div>
   </section>

0686e07a8ecd95ef894ad905c3c82f41140df85b
Zachary Nemeroff <zanemeroff@gmail.com>
2020-07-22 15:08:24 -0600

    Add file

---
+++ src/components/TeamDisplay/TeamDisplay.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>team dispaly</h1>
+</template>
+
+<script>
+  export default {
+    name: 'TeamDisplay',
+    props: {},
+    data() {
+      return {
+        // code
+      }
+    },
+    methods: {}
+  }
+</script>
+
+<style>
+  @import './TeamDisplay.css';
+</style>

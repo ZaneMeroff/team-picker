cfdf7263d66a88b268a92717ebc17e8d8840a79b
Zachary Nemeroff <zanemeroff@gmail.com>
2020-07-22 00:56:52 -0600

    Add file

---
+++ src/components/PlayerNameInput/PlayerNameInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- <section class='player-name-input-outer-container'> -->
+    <input placeholder='player name...' class='player-name-input'/>
+  <!-- </section> -->
+</template>
+
+<script>
+  export default {
+    name: 'PlayerNameInput',
+    props: {},
+    data() {
+      return {
+        // code
+      }
+    },
+    methods: {}
+  }
+</script>
+
+<style>
+  @import './PlayerNameInput.css';
+</style>

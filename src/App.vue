ec1f23d6d34a19f301ac83a6a559f4230b9f1058
Zachary Nemeroff <zanemeroff@gmail.com>
2020-07-22 18:46:11 -0600

    Create genearteTeams

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
       <div v-if='currentStep === 1'>
         <NumSelector label='teams' :number='numberOfTeams' :updateNumber='updateNumber'/>
         <NumSelector label='players' :number='playerNames.length' :updateNumber='updateNumber'/>
+        <button class='next-button' @click="nextScreen('next')">next</button>
       </div>
 
       <div v-if='currentStep === 2'>
@@ -13,13 +14,16 @@
         <div v-for='(player, index) in playerNames' :key='index' >
           <PlayerNameInput :id='index' :updateNames='updateNames'/>
         </div>
+        <button class='next-button' @click="generateTeams(playerNames, numberOfTeams)">GO!</button>
       </div>
 
-      <div v-if='currentStep === 3'>
-        <TeamDisplay />
-      </div>
+      <!-- <div v-if='currentStep === 3'>
+        <div v-for='(team, index) in '>
+          <TeamDisplay :id='index' :playerNames='playerNames' />
+        </div>
+      </div> -->
 
-      <button class='next-button' @click="nextScreen('next')">next</button>
+
     </div>
   </div>
 </template>
@@ -27,22 +31,54 @@
 <script>
   import NumSelector from './components/NumSelector/NumSelector.vue';
   import PlayerNameInput from './components/PlayerNameInput/PlayerNameInput.vue';
-  import TeamDisplay from './components/TeamDisplay/TeamDisplay.vue';
+  // import TeamDisplay from './components/TeamDisplay/TeamDisplay.vue';
   export default {
     name: 'App',
     components: {
       NumSelector,
       PlayerNameInput,
-      TeamDisplay
+      // TeamDisplay
     },
     data() {
       return {
         numberOfTeams: 2,
         playerNames: ['',''],
-        currentStep: 3
+        teamObjects: [],
+        currentStep: 1
       }
     },
     methods: {
+      generateTeams(names, numOfTeams) {
+        let numPerTeam = Math.floor(names.length / numOfTeams)
+        let teamObjArray = []
+        for (let i = 1; i <= numOfTeams; i++) {
+          let teamObj = { teamID: i, names: [] }
+          teamObjArray.push(teamObj)
+        }
+        teamObjArray.forEach(team => {
+          names.forEach(name => {
+            console.log(name);
+            if (team.names.length < numPerTeam) {
+              let targetName = names.pop()
+              team.names.push(targetName)
+            }
+          })
+        })
+        if (names.length) {
+          let sortedTeams = teamObjArray.sort((a,b) => a.names.length - b.names.length)
+          names.forEach(extraName => {
+            console.log(extraName);
+            sortedTeams.forEach(openSpot => {
+              if (names.length) {
+                let targetExtra = names.pop()
+                openSpot.names.push(targetExtra)
+              }
+            })
+          })
+        }
+        this.nextScreen('next');
+        this.teamObjects = teamObjArray;
+      },
       nextScreen(nextOrBack) {
         if (nextOrBack === 'next') {
           this.currentStep++

0ea93216af436f5f6688c51f360e360fbf9793fe
Zachary Nemeroff <zanemeroff@gmail.com>
2020-07-28 12:08:35 -0600

    Refactor config

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  preset: '@vue/cli-plugin-unit-jest'
+}
+
+// module.exports = {
+//   preset: '@vue/cli-plugin-unit-jest',
+//   verbose: true,
+//   roots: ["<rootDir>/src/", "<rootDir>/src/"],
+//   moduleFileExtensions: ['js', 'vue'],
+//   moduleNameMapper: {
+//     '^@/(.*)$': '<rootDir>/src/$1',
+//   },
+//   transform: {
+//     "^.+\\.js$": "babel-jest",
+//   },
+//   snapshotSerializers: [
+//     "<rootDir>/node_modules/jest-serializer-vue"
+//   ]
+// }

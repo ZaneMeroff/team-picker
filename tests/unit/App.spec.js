7afd5b0cc7076c34c41d6d60588fbe7c254150cc
Zachary Nemeroff <zanemeroff@gmail.com>
2020-07-28 14:01:43 -0600

    Add tests

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -2,9 +2,31 @@ import { shallowMount } from '@vue/test-utils'
 import App from '@/App.vue'
 
 describe('App.vue', () => {
-  it('does something...', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(App)
-    expect(msg).toEqual('new message')
+
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(App)
   })
+
+  it('component render matches snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('component has correct default data values', () => {
+    expect(wrapper.vm.$data.numberOfTeams).toEqual(2)
+    expect(wrapper.vm.$data.playerNames).toEqual(['',''])
+    expect(wrapper.vm.$data.teamObjects).toEqual([])
+    expect(wrapper.vm.$data.currentStep).toEqual(1)
+    expect(wrapper.vm.$data.playerCountErrorMessage).toEqual(false)
+    expect(wrapper.vm.$data.nameInputsErrorMessage).toEqual(false)
+  })
+
+  it('buildTeamObjects creates correct number of teams', () => {
+    expect(wrapper.vm.$data.teamObjects.length).toEqual(0)
+    wrapper.vm.buildTeamObjects(2)
+    expect(wrapper.vm.$data.teamObjects.length).toEqual(2)
+  })
+
+
 })
